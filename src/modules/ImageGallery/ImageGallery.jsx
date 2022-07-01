df667d726c8d6ccdc4f796753f5928da3537e20a
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-07-01 14:46:27 +0300

    Starts files

---
+++ src/modules/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+import styles from './ImageGallery.module.css';
+
+class ImageGallery extends Component {
+    render() {
+    
+        
+    }
+        return ()
+}
+
+export default ImageGallery
